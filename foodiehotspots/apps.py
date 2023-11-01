f852ada27dc2eba6ab5f0648771042baa1d2ed05
sgaeng <ksge1124@gmail.com>
2023-11-01 13:22:03 +0900

    :tada:Init: poetry로 버전관리 및 django&mysql 개발환경세팅  #1

--- foodiehotspots/apps.py
+++ foodiehotspots/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class FoodiehotspotConfig(AppConfig):
+class FoodiehotspotsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'foodiehotspot'
+    name = 'foodiehotspots'
