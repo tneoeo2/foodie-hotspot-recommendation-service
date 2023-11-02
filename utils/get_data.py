0659310d6f7d3baea78c97ebc81ed1e16e54c382
tneoeo2 <tneoeo2@gmail.com>
2023-11-02 23:26:37 +0900

    Revert "################"
    This reverts commit 29ccdd3d820b5bad6905fa0be146338e132d4d27.

--- utils/get_data.py
+++ utils/get_data.py
@@ -1,14 +1,11 @@
 import time
 import json
-import copy
 import requests
 import environ
 import xmltodict
-# from django.conf import settings
 from custom_logger import CustomLogger
 
-logger = CustomLogger("INFO").get_logger()  # 테스트용 추후변경
-# logger = settings.CUSTOM_LOGGER
+logging = CustomLogger("DEBUG").get_logger()  # 테스트용 추후변경
 
 env = environ.Env(DEBUG=(bool, True))
 
@@ -72,11 +69,11 @@ def get_restaurant(api_url, api_key, page_index:int, page_size:int)->dict:   # A
         root = response.text
         jsondata = json.dumps(xmltodict.parse(root), indent=4)
         
-        jsondict = json.loads(jsondata)
-        logger.debug(f"jsondict: {jsondict}")
+        dict = json.loads(jsondata)
+        # logging.debug(f"dict: {dict}")
         
 
-    return jsondict
+    return dict
 
 
 def get_mapping_data(raw_data:dict, field:dict) -> dict:
@@ -85,85 +82,62 @@ def get_mapping_data(raw_data:dict, field:dict) -> dict:
     :param field:  기존필드명, 변경할 필드명이 담긴 dict
     :return: 매핑된 데이터 
     '''
-    result_list = []
-    result_dict = {}
-    logger.debug(f'raw_data : {raw_data}')
-    if type(raw_data) != list:
-        tmp = copy.deepcopy(result_dict)
-        for key, value in raw_data.items():
-            new_key = field[key]  
-            tmp[new_key] = value
-        result_list.append(tmp)
-    else: 
+    result = {}
+    if len(raw_data) != 1:
     # 기존 데이터의 키를 DB_FIELD 딕셔너리를 참조하여 교체
         for el in raw_data:
-            tmp = copy.deepcopy(result_dict)
             for key, value in el.items():
                 new_key = field[key]  
-                tmp[new_key] = value
-            result_list.append(tmp)
-                
+                result[new_key] = value
+    else: 
+        for key, value in raw_data.items():
+            new_key = field[key]  
+            result[new_key] = value
 
-    return result_list
+    return result
 
-def processing_data(page_index:int=1, page_size:int=500, total:int=None)->dict:  #! 추후 수정 예정
-    '''
-    :param page_index:  페이지 번호
-    :param page_size:  페이지에 담긴 정보 수 
-    :param total:  가져올 데이터 양 - None 설정시 전체 데이터 반환
-    :return: 매핑된 데이터 
-    '''
-    
+
+try:
+    start_time1 = time.time()
     new_data = [] #결과 데이터
-    try:
-        start_time1 = time.time()
+     
+    for key in API_URL.keys(): # 모든 url에 요청
+        start_time2 = time.time()
+        unprocessed_data = -1 #남은 데이터 숫자 확인용
+        #* 아래 두 변수는 필요에 따라 수정하여 사용합니다.
+        page_index = 1  # 페이지 번호
+        page_size = 1000  # 페이지에 담긴 정보 수 
         
-        for key in API_URL.keys(): # 모든 url에 요청
-            start_time2 = time.time()
-            unprocessed_data = -1 #남은 데이터 숫자 확인용
-            #* 아래 두 변수는 필요에 따라 수정하여 사용합니다.
-            pindex = page_index  # 페이지 번호
-            psize = page_size  # 페이지에 담긴 정보 수 
-            
-            logger.info(f'----------{key} : {API_URL[key]} API 요청 시작----------')
-            
-            while unprocessed_data > 0 or unprocessed_data == -1:
-                raw_data = get_restaurant(API_URL[key], API_KEY, page_index,  page_size)  #? API 요청
-                try:
-                    status = raw_data[API_URL[key].split('/')[-1]]['head']['RESULT']['CODE'] #* CODE:INFO-000시 정상, 그 외 API 요청 실패처리 
-                    status_code = status.split('-')[-1]
-                    if status_code == '000' :   # 요청 성공
-                        tot_cnt = int(raw_data[API_URL[key].split('/')[-1]]['head']['list_total_count'])
-                        if total == None:
-                            unprocessed_data =  tot_cnt
-                        else: 
-                            unprocessed_data =  total
-                        unprocessed_data -= psize*pindex
-                        unprocessed_data = max(unprocessed_data, 0)
-                        logger.info(f'남은 데이터 수 : {unprocessed_data}')
-                        pindex += 1
-                        new_data.append(get_mapping_data(raw_data[API_URL[key].split('/')[-1]]['row'], DB_FIELD)) #? 데이터 매핑
-                except Exception as e:
-                    logger.error(f'API 요청 실패 --- {raw_data}\n {e}')
-                    logger.info(f'time_check: {time.time()-start_time2}')
-                    raise Exception('API 요청 실패 : 에러 코드를  확인하세요')
-                    # status = raw_data['RESULT']['CODE']   #* 에러코드 반환시 형식
-                    # status_code = status.split('-')[-1]
-                    # if status_code != '000' :
-                # if page_index == 5:   #! 테스트용 제한
-            #? 각 URL별 결과반환
-            logger.debug(f'new_data : {new_data}  리스트 길이 :  {len(new_data)}, 전체 데이터 수 : {tot_cnt}')
-            logger.info(f'{key}_time_check: {time.time()-start_time2}')
-            logger.info('==========*****==========\n')
-        #? 전체 URL 결과 반환
-        logger.debug(f'new_data : {new_data}  리스트 길이 :  {len(new_data)}, 전체 데이터 수 : {tot_cnt}')
-        logger.info(f'total_time_check: {time.time()-start_time1}')
+        logging.info(f'----------{key} : {API_URL[key]} API 요청 시작----------')
+        
+        while unprocessed_data > 0 or unprocessed_data == -1:
+            raw_data = get_restaurant(API_URL[key], API_KEY, page_index,  page_size)  #? API 요청
+            try:
+                status = raw_data[API_URL[key].split('/')[-1]]['head']['RESULT']['CODE'] #* CODE:INFO-000시 정상, 그 외 API 요청 실패처리 
+                status_code = status.split('-')[-1]
+                if status_code == '000' :   # 요청 성공
+                    tot_cnt = int(raw_data[API_URL[key].split('/')[-1]]['head']['list_total_count'])
+                    unprocessed_data =  tot_cnt
+                    unprocessed_data -= page_size*page_index
+                    unprocessed_data = max(unprocessed_data, 0)
+                    logging.debug(f'남은 데이터 수 : {unprocessed_data}')
+                    page_index += 1
+                    new_data.append(get_mapping_data(raw_data[API_URL[key].split('/')[-1]]['row'], DB_FIELD)) #? 데이터 매핑
+            except Exception as e:
+                logging.error(f'API 요청 실패 --- {raw_data}\n {e}')
+                logging.info(f'time_check: {time.time()-start_time2}')
+                raise Exception('API 요청 실패 : 에러 코드를  확인하세요')
+                # status = raw_data['RESULT']['CODE']   #* 에러코드 반환시 형식
+                # status_code = status.split('-')[-1]
+                # if status_code != '000' :
+            # if page_index == 5:   #! 테스트용 제한
+        logging.debug(f'new_data : {new_data}  리스트 길이 :  {len(new_data)}, 전체 데이터 수 : {tot_cnt}')
+        logging.info(f'{key}_time_check: {time.time()-start_time2}')
+        logging.debug('==========*****==========\n')
+    logging.debug(f'new_data : {new_data}  리스트 길이 :  {len(new_data)}, 전체 데이터 수 : {tot_cnt}')
+    logging.info(f'total_time_check: {time.time()-start_time1}')
 
-    except Exception as e:
-            logger.info(f'{key}_time_check: {time.time()-start_time2}')
-            logger.error(f'ERROR : {e}')    
+except Exception as e:
+        logging.info(f'{key}_time_check: {time.time()-start_time2}')
+        logging.error(f'ERROR : {e}')    
 
-    return new_data
-
-
-# processing_data()
