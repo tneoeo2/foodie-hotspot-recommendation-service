dd13ebc0203cbd477ef460d5855b6d51903ce910
sgaeng <ksge1124@gmail.com>
2023-11-07 17:59:55 +0900

    :sparkles:Feat : redis 이용해서 캐싱 구현  #19

--- accounts/views.py
+++ accounts/views.py
@@ -1,42 +1,27 @@
-import requests
-import jwt 
+import jwt
 
 from django.conf import settings
-from django.contrib.auth import get_user_model
-
-from .models import User
+from django.core.cache import cache
 from django.shortcuts import get_object_or_404
-import requests
-from rest_framework_simplejwt.authentication import JWTAuthentication
-
-from django.shortcuts import render
-from django.shortcuts import get_object_or_404
+from django.views.decorators.cache import cache_page
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.generics import RetrieveUpdateAPIView, ListAPIView
 from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework_simplejwt.authentication import JWTAuthentication
 
 from accounts.serializers import UserDetailUpdateSerializers, LocationSerializers
-from accounts.models import Location
+from accounts.models import User, Location
 from utils.location import location_load
 
 
 SECRET_KEY = settings.SECRET_KEY
 ALGORITHM = 'HS256'
-# Create your views here.
+
+
 class UserDetailsView(RetrieveUpdateAPIView):
-    """
-    Reads and updates UserModel fields
-    Accepts GET, PUT, PATCH methods.
-
-    Default accepted fields: username, first_name, last_name
-    Default display fields: pk, username, email, first_name, last_name
-    Read-only fields: pk, email
-
-    Returns UserModel fields.
-    """
-    serializer_class = UserDetailUpdateSerializers
     permission_classes = [IsAuthenticated]
+    serializer_class = UserDetailUpdateSerializers
 		
     # JWT 인증방식 클래스 지정하기
     authentication_classes = [JWTAuthentication]
@@ -47,32 +32,66 @@ class UserDetailsView(RetrieveUpdateAPIView):
         data = jwt.decode(token_str, SECRET_KEY, ALGORITHM)
         obj = get_object_or_404(User, id=data['user_id'])
         return obj
+
+
+# class LocationListView(ListAPIView):
+#     permission_classes = [AllowAny]
+#     # permission_classes = [IsAuthenticated]
+
+#     serializer_class = LocationSerializers
     
+#     def get_queryset(self):
+#         # 시도 및 시군구 목록을 캐시에서 가져오기
+#         cached_data = cache.get('location_data')
+#         if cached_data:
+#             print(cached_data)
+#             return cached_data
+
+#         queryset = Location.objects.all()
+#         query_params = self.request.query_params
+#         if not query_params:
+#             return queryset
+        
+#         do_si = query_params.get("dosi", None)
+#         if do_si:
+#             queryset = queryset.filter(dosi=do_si)
+        
+#         sgg = query_params.get("sgg", None)
+#         if sgg:
+#             queryset = queryset.filter(sgg=sgg)
+        
+#         # 데이터를 캐시에 저장하고 반환
+#         cache.set('location_data', queryset)
+        
+#         return queryset
 
 
-
-class LocationListView(ListAPIView):
+class LocationListView(APIView):
     permission_classes = [AllowAny]
-    # permission_classes = [IsAuthenticated]
     
-    queryset = Location.objects.all()
-    serializer_class = LocationSerializers
-    
-    def get_queryset(self):
-        queryset = Location.objects.all()
-        query_params = self.request.query_params
-        if not query_params:
-            return queryset
+    def get(self, request):
+        # 시도 및 시군구 목록을 캐시에서 검색
+        cached_data = cache.get('location_data')
+        if cached_data is not None:
+            return Response(cached_data)  # set() 으로 저장할 때 이미 serializer화 된 상태로 저장됨
         
-        do_si = query_params.get("do_si", None)
+
+        do_si = request.query_params.get("dosi", None)
+        sgg = request.query_params.get("sgg", None)
+
         if do_si:
-            queryset = queryset.filter(dosi=do_si)
-        
-        sgg = query_params.get("sgg", None)
-        if sgg:
-            queryset = queryset.filter(sgg=sgg)
-        
-        return queryset
+            filtered_objects = Location.objects.filter(dosi=do_si)
+        elif sgg:
+            filtered_objects = Location.objects.filter(sgg=sgg)
+        else:
+            filtered_objects = Location.objects.all()
+
+        serializer = LocationSerializers(filtered_objects, many=True)
+
+        # 데이터를 캐시에 저장 (무기한 유지)
+        cache.set('location_data', serializer.data)
+
+        return Response(serializer.data)
 
 
 class testAPI(APIView):
@@ -82,5 +101,3 @@ class testAPI(APIView):
         # location_load.load_to_db()
         
         return Response({"message": "this is testAPI"})
-
- 
