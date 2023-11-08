85669f8ba424854e1279c0fa35666636100fc42b
tneoeo2 <tneoeo2@gmail.com>
2023-11-09 01:45:33 +0900

    :bug:Fix : 버그 수정
    -accounts.urls.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,18 +1,10 @@
 from django.urls import path
-<<<<<<< HEAD
-from .views import UserDetailsView
-=======
 from accounts import views
->>>>>>> df5e2641355519a0f2b41de1888e6225cd00d71b
 
 app_name = "accounts"
 
 urlpatterns =[
-<<<<<<< HEAD
-    path("", UserDetailsView.as_view()),
-=======
     path("", views.UserDetailsView.as_view()),
     path("locations/", views.LocationListView.as_view()),
     path("test/", views.testAPI.as_view()),
->>>>>>> df5e2641355519a0f2b41de1888e6225cd00d71b
 ]
