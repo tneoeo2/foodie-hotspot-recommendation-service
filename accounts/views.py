7be81b34e706d54f8799a5135aab6107f5aae40f
tneoeo2 <tneoeo2@gmail.com>
2023-11-08 23:53:30 +0900

    :bug:Fix : 버그 수정  - get_object의 request 문제 해결  - get_queryset 정의 문제  - serializers field 오기입
    - 코드 충돌 수정

--- accounts/views.py
+++ accounts/views.py
@@ -1,28 +1,20 @@
-import requests
-import jwt 
-
+from django.shortcuts import render
+from django.core.cache import cache
+from rest_framework.generics import RetrieveUpdateAPIView, ListAPIView
 from django.conf import settings
+from .models import Location
 from django.contrib.auth import get_user_model
-
-from .models import User
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from  .serializers import UserDetailUpdateSerializers, LocationSerializers
 from django.shortcuts import get_object_or_404
 import requests
 import logging
-from rest_framework_simplejwt.authentication import JWTAuthentication
-from django.db.models.query import prefetch_related_objects
-from django.shortcuts import render
-from django.shortcuts import get_object_or_404
-from rest_framework.response import Response
-from rest_framework.views import APIView
-from rest_framework.generics import RetrieveUpdateAPIView, ListAPIView
-from rest_framework.permissions import AllowAny, IsAuthenticated
-
-from accounts.serializers import UserDetailUpdateSerializers, LocationSerializers
-from accounts.models import Location
-from utils.location import location_load
+import jwt 
 
 SECRET_KEY = settings.SECRET_KEY
 ALGORITHM = 'HS256'
+
+
 # Create your views here.
 class UserDetailsView(RetrieveUpdateAPIView):
     """
@@ -40,7 +32,7 @@ class UserDetailsView(RetrieveUpdateAPIView):
 		
     # JWT 인증방식 클래스 지정하기
     authentication_classes = [JWTAuthentication]
-    queryset = User.objects.all()
+
 
     def get_object(self, request):
         token_str = request.headers.get("Authorization").split(' ')[1]
@@ -48,40 +40,25 @@ class UserDetailsView(RetrieveUpdateAPIView):
         obj = get_object_or_404(User, id=data['user_id'])
         return obj
     
-    def retrieve(self, request, *args, **kwargs):
-        instance = self.get_object(request)
-        serializer = self.get_serializer(instance)
-        return Response(serializer.data)
-    
-    def update(self, request, *args, **kwargs):
-        partial = kwargs.pop('partial', False)
-        instance = self.get_object(request)
-        serializer = self.get_serializer(instance, data=request.data, partial=partial)
-        serializer.is_valid(raise_exception=True)
-        self.perform_update(serializer)
-
-        queryset = self.filter_queryset(self.get_queryset())
-        if queryset._prefetch_related_lookups:
-            # If 'prefetch_related' has been applied to a queryset, we need to
-            # forcibly invalidate the prefetch cache on the instance,
-            # and then re-prefetch related objects
-            instance._prefetched_objects_cache = {}
-            prefetch_related_objects([instance], *queryset._prefetch_related_lookups)
-
-        return Response(serializer.data)
 
 
 
-
-class LocationListView(ListAPIView):
+class LocationListView(ListAPIView):  #캐싱 적용
     permission_classes = [AllowAny]
     # permission_classes = [IsAuthenticated]
     
-    queryset = Location.objects.all()
+    # queryset = Location.objects.all()
+    queryset = cache.get('locations')
+    if queryset is None:
+        queryset = Location.objects.all()
     serializer_class = LocationSerializers
     
     def get_queryset(self):
-        queryset = Location.objects.all()
+        # queryset = Location.objects.all()
+        queryset = cache.get('locations')
+        if queryset is None:
+            queryset = Location.objects.all()
+            cache.set('locations', queryset)
         query_params = self.request.query_params
         if not query_params:
             return queryset
@@ -97,12 +74,32 @@ class LocationListView(ListAPIView):
         return queryset
 
 
-class testAPI(APIView):
-    permission_classes = [AllowAny]
+# class testAPI(APIView):
+#     permission_classes = [AllowAny]
     
-    def get(self, request):
-        # location_load.load_to_db()
+#     def get(self, request):
+#         # location_load.load_to_db()
         
-        return Response({"message": "this is testAPI"})
+#         return Response({"message": "this is testAPI"})
 
- 
+# class testAPI(ListAPIView):    #캐싱 적용 안됨
+#     permission_classes = [AllowAny]
+    
+#     queryset = Location.objects.all()
+#     serializer_class = LocationSerializers
+    
+#     def get_queryset(self):
+#         queryset = Location.objects.all()
+#         query_params = self.request.query_params
+#         if not query_params:
+#             return queryset
+        
+#         do_si = query_params.get("do_si", None)
+#         if do_si:
+#             queryset = queryset.filter(dosi=do_si)
+        
+#         sgg = query_params.get("sgg", None)
+#         if sgg:
+#             queryset = queryset.filter(sgg=sgg)
+        
+#         return queryset
